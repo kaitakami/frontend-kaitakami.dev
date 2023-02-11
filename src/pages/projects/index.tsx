0fc0bd3286401a2d8162f0e67219ad21d965b6de
Kai Takami <kaitakami@protonmail.com>
2023-02-11 20:24:50 +0900

    Create page for projects

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+import HeadLayout from "@/components/Layout/Head"
+import Animate from "@/components/Layout/Animate"
+import Layout from "@/components/Layout/Layout"
+const index = () => {
+  return (
+    <>
+      <HeadLayout title="Projects" />
+      <Animate>
+        <>
+          <Layout>
+            <>
+              <div className="max-w-2xl px-5 md:px-0 m-auto flex flex-col gap-6">
+                <h1 className="text-3xl font-bold">Projects</h1>
+                <p>This website is still in construction :)</p>
+                <Link href="/">Go back</Link>
+              </div>
+            </>
+          </Layout>
+        </>
+      </Animate>
+      </>
+  )
+}
+
+export default index
