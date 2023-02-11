f7de4c319f3160a0d3a2af307653f6c7be882314
Kai Takami <kaitakami@protonmail.com>
2023-02-11 21:34:15 +0900

    Add some design to the projects and blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link"
 import Animate from "@/components/Layout/Animate"
+import HeadLayout from "@/components/Layout/Head"
+import Layout from "@/components/Layout/Layout"
 
 const blog = () => {
   return (
     <>
+      <HeadLayout title="Blog" />
       <Animate>
         <>
-          <div>This is the blog</div>
-          <Link href="/">Go back</Link>
+          <Layout>
+            <>
+              <div className="max-w-2xl px-5 md:px-0 m-auto flex flex-col gap-6">
+                <h1 className="text-3xl font-bold">Blog</h1>
+                <p>This website is still in construction :)</p>
+                <Link href="/">Go back</Link>
+              </div>
+            </>
+          </Layout>
         </>
       </Animate>
     </>
